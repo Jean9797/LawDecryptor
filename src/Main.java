4f0964b9ea5840e608faa0cc840000e9d3875f91
Jan Rodzoń <rodzonjan97@gmail.com>
2017-12-22 23:04:25 +0100

    Exceptions hided. Only exceptions messages are printed on the screen.

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,32 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws ParseException, IOException{
-        OptionManager optionManager = new OptionManager();
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(optionManager.createOptions(), args);
+        try {
+            OptionManager optionManager = new OptionManager();
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(optionManager.createOptions(), args);
 
-        optionManager.checkCorrectnessOfTheArguments(cmd);
+            optionManager.checkCorrectnessOfTheArguments(cmd);
 
-        if (cmd.hasOption("h")){
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp( "java -cp bin:commons-cli-1.4/commons-cli-1.4.jar Main", optionManager.getOptions() , true);
-            return;
+            if (cmd.hasOption("h")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("java -cp bin:commons-cli-1.4/commons-cli-1.4.jar Main", optionManager.getOptions(), true);
+                return;
+            }
+
+            FileScanner file = new FileScanner();
+            List<String> text = file.read(cmd.getOptionValue("f"));
+            Preparser preparser = new Preparser();
+            List<String> preparedText = preparser.preparse(text);
+            StructureBuilder builder = new StructureBuilder();
+            for (String line : preparedText) {
+                builder.build(line);
+            }
+            Statute statute = builder.getStatute();
+            optionManager.executeSelectedOptions(cmd, statute);
         }
-
-        FileScanner file = new FileScanner();
-        List<String> text = file.read(cmd.getOptionValue("f"));
-        Preparser preparser = new Preparser();
-        List<String> preparedText = preparser.preparse(text);
-        StructureBuilder builder = new StructureBuilder();
-        for (String line : preparedText){
-            builder.build(line);
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
         }
-        Statute statute = builder.getStatute();
-        optionManager.executeSelectedOptions(cmd, statute);
     }
 }
